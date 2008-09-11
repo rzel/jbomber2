9bdb1f423dff2e4859ca7a31a9c07e55dddf7c3b
daniel.geelen <daniel.geelen@b7039a8d-002b-0410-823b-f75e9e743494>
2008-09-11 10:18:16 +0000

    Move files from 'smoke' namespace to regular no-namespace

--- jsmoke/src/SampleCallCLib.java
+++ jsmoke/src/SampleCallCLib.java
@@ -1,4 +1,3 @@
-package smoke;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
@@ -6,20 +5,20 @@ import com.sun.jna.Pointer;
  *
  * @author huub
  */
-public class SampleCallCLib {    
+public class SampleCallCLib {
     private static MyNativeLibrary INSTANCE;
-    
+
     static {
         INSTANCE = (MyNativeLibrary)Native.loadLibrary("c",MyNativeLibrary.class);
     }
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.err.println(INSTANCE.atol("123"));
     }
-    
+
 }
 
 interface MyNativeLibrary extends Library {
